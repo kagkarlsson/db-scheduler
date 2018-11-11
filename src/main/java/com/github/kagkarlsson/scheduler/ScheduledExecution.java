b7307adc693955d8805de4aebcb5fa86ee619090
Gustav Karlsson <gustav.karlsson@bekk.no>
2018-11-11 20:29:54 +0100

    Addings equals, hashcode for ScheduledExecution

--- src/main/java/com/github/kagkarlsson/scheduler/ScheduledExecution.java
+++ src/main/java/com/github/kagkarlsson/scheduler/ScheduledExecution.java
@@ -14,42 +14,42 @@
  * limitations under the License.
  */
 package com.github.kagkarlsson.scheduler;
-
 import com.github.kagkarlsson.scheduler.task.Execution;
 import com.github.kagkarlsson.scheduler.task.TaskInstance;
 import com.github.kagkarlsson.scheduler.task.TaskInstanceId;
-
 import java.time.Instant;
-
+import java.util.Objects;
 public class ScheduledExecution<DATA_TYPE> {
-
     private final Class<DATA_TYPE> dataClass;
-
     private final Execution execution;
-
     public ScheduledExecution(Class<DATA_TYPE> dataClass, Execution execution) {
         this.dataClass = dataClass;
         this.execution = execution;
     }
-
     public TaskInstanceId getTaskInstance() {
         return execution.taskInstance;
     }
-
     public Instant getExecutionTime() {
         return execution.getExecutionTime();
     }
-
     @SuppressWarnings("unchecked")
     public DATA_TYPE getData() {
         if (dataClass.isInstance(this.execution.taskInstance.getData())) {
             return (DATA_TYPE) this.execution.taskInstance.getData();
         }
-
         throw new DataClassMismatchException();
     }
-
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScheduledExecution<?> that = (ScheduledExecution<?>) o;
+        return Objects.equals(execution, that.execution);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(execution);
+    }
     public static class DataClassMismatchException extends RuntimeException {
-
     }
 }
