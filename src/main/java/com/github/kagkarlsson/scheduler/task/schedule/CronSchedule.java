acf00ff63b8024c4d4088f8780d6e3cbe8502c44
Gustav Karlsson <gustav.karlsson@bekk.no>
2019-03-17 20:11:28 +0100

    Cron-support using shaded com.cronutils library.

---
+++ src/main/java/com/github/kagkarlsson/scheduler/task/schedule/CronSchedule.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) Gustav Karlsson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.kagkarlsson.scheduler.task.schedule;
+
+import com.cronutils.model.Cron;
+import com.cronutils.model.CronType;
+import com.cronutils.model.definition.CronDefinitionBuilder;
+import com.cronutils.model.time.ExecutionTime;
+import com.cronutils.parser.CronParser;
+import com.github.kagkarlsson.scheduler.task.ExecutionComplete;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+
+/**
+ * Spring-style cron-pattern schedule
+ */
+public class CronSchedule implements Schedule {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CronSchedule.class);
+    private final ExecutionTime cronExecutionTime;
+
+    public CronSchedule(String pattern) {
+        CronParser parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.SPRING));
+        Cron cron = parser.parse(pattern);
+        this.cronExecutionTime = ExecutionTime.forCron(cron);
+    }
+
+    @Override
+    public Instant getNextExecutionTime(ExecutionComplete executionComplete) {
+        Optional<ZonedDateTime> nextTime = cronExecutionTime.nextExecution(ZonedDateTime.ofInstant(executionComplete.getTimeDone(), ZoneId.systemDefault()));
+        if (!nextTime.isPresent()) {
+            LOG.error("Cron-pattern did not return any further execution-times. This behavior is currently not supported by the scheduler. Setting next execution-time to far-future.");
+            return Instant.now().plus(1000, ChronoUnit.YEARS);
+        }
+        return nextTime.get().toInstant();
+    }
+}
