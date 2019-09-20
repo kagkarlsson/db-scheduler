c61aa9c1e473146e81e5af0c7dbdaa83e7c5bea1
Paul Hilliar <paul.hilliar@velopayments.com>
2019-09-20 17:43:11 +0100

    Support time zones in CronExpression so that schedules can fire at a time of day that moves specific to the explicit zone not the zone of the JVM.

--- db-scheduler/src/main/java/com/github/kagkarlsson/scheduler/task/schedule/CronSchedule.java
+++ db-scheduler/src/main/java/com/github/kagkarlsson/scheduler/task/schedule/CronSchedule.java
@@ -28,6 +28,7 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -37,16 +38,29 @@ public class CronSchedule implements Schedule {
 
     private static final Logger LOG = LoggerFactory.getLogger(CronSchedule.class);
     private final ExecutionTime cronExecutionTime;
+    private final ZoneId zoneId;
 
-    public CronSchedule(String pattern) {
+    public CronSchedule(String pattern, ZoneId zoneId) {
         CronParser parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.SPRING));
         Cron cron = parser.parse(pattern);
         this.cronExecutionTime = ExecutionTime.forCron(cron);
+
+        if (zoneId == null) {
+            throw new IllegalArgumentException("zoneId may not be null");
+        }
+        this.zoneId = zoneId;
+    }
+
+    public CronSchedule(String pattern) {
+        this(pattern, ZoneId.systemDefault());
     }
 
     @Override
     public Instant getNextExecutionTime(ExecutionComplete executionComplete) {
-        Optional<ZonedDateTime> nextTime = cronExecutionTime.nextExecution(ZonedDateTime.ofInstant(executionComplete.getTimeDone(), ZoneId.systemDefault()));
+        ZonedDateTime lastDone = ZonedDateTime.ofInstant(executionComplete.getTimeDone(), zoneId);  //frame the 'last done' time in the context of the time zone for this schedule
+        //so that expressions like "0 05 13,20 * * ?" (New York) can operate in the
+        // context of the desired time zone
+        Optional<ZonedDateTime> nextTime = cronExecutionTime.nextExecution(lastDone);
         if (!nextTime.isPresent()) {
             LOG.error("Cron-pattern did not return any further execution-times. This behavior is currently not supported by the scheduler. Setting next execution-time to far-future.");
             return Instant.now().plus(1000, ChronoUnit.YEARS);
