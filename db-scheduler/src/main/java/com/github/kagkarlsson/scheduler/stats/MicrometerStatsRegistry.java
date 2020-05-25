ca95fa5fe7b164b57633d8c9a6c5b95caf9357a5
Even Holthe <even.holthe@me.com>
2020-05-25 14:10:59 +0200

    Make MicrometerStatsRegistry public

--- db-scheduler/src/main/java/com/github/kagkarlsson/scheduler/stats/MicrometerStatsRegistry.java
+++ db-scheduler/src/main/java/com/github/kagkarlsson/scheduler/stats/MicrometerStatsRegistry.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) Gustav Karlsson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.kagkarlsson.scheduler.stats;
 
 import com.github.kagkarlsson.scheduler.task.ExecutionComplete;
@@ -12,7 +27,7 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
-class MicrometerStatsRegistry implements StatsRegistry {
+public class MicrometerStatsRegistry implements StatsRegistry {
 
     private static final String RESULT_SUCCESS = "ok";
     private static final String RESULT_FAILURE = "failed";
@@ -20,7 +35,7 @@ class MicrometerStatsRegistry implements StatsRegistry {
 
     private Map<String, MetricsHolder> metricsMap = new HashMap<>();
 
-    MicrometerStatsRegistry(MeterRegistry meterRegistry, List<? extends Task<?>> expectedTasks) {
+    public MicrometerStatsRegistry(MeterRegistry meterRegistry, List<? extends Task<?>> expectedTasks) {
         this.meterRegistry = meterRegistry;
         initializeMetricsForAllTasks(expectedTasks);
     }
