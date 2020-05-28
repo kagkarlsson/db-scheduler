088b3c8879228719a4191feccd3e7c8a803fddd3
Even Holthe <even.holthe@me.com>
2020-05-28 12:54:35 +0200

    Attempt to revive the OracleCompabilityTest

---
+++ db-scheduler/src/test/java/com/github/kagkarlsson/scheduler/compatibility/Oracle11gCompatibilityTest.java
@@ -0,0 +1,39 @@
+package com.github.kagkarlsson.scheduler.compatibility;
+
+import com.github.kagkarlsson.scheduler.DbUtils;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.util.DriverDataSource;
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.testcontainers.containers.OracleContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Tag("compatibility")
+@Testcontainers
+public class Oracle11gCompatibilityTest extends CompatibilityTest {
+    @Container
+    private static final OracleContainer ORACLE = new OracleContainer("oracleinanutshell/oracle-xe-11g:1.0.0");
+    private static HikariDataSource pooledDatasource;
+
+    @BeforeAll
+    private static void initSchema() {
+        final DriverDataSource datasource = new DriverDataSource(ORACLE.getJdbcUrl(), "oracle.jdbc.OracleDriver", new Properties(), ORACLE.getUsername(), ORACLE.getPassword());
+
+        final HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDataSource(datasource);
+        hikariConfig.setMaximumPoolSize(10);
+        pooledDatasource = new HikariDataSource(hikariConfig);
+
+        // init schema
+        DbUtils.runSqlResource("/oracle_tables.sql").accept(pooledDatasource);
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        return pooledDatasource;
+    }
+}
--- db-scheduler/src/test/resources/oracle_tables.sql
+++ db-scheduler/src/test/resources/oracle_tables.sql
@@ -1,14 +1,15 @@
-create table scheduled_tasks (
-    task_name varchar(100),
-    task_instance varchar(100),
-    task_data blob,
-    execution_time TIMESTAMP(6),
-    picked NUMBER(1,0),
-    picked_by varchar(50),
-    last_success TIMESTAMP(6),
-    last_failure TIMESTAMP(6),
-    consecutive_failures NUMBER(19,0),
-  last_heartbeat TIMESTAMP(6),
-    version NUMBER(19,0),
+create table scheduled_tasks
+(
+    task_name            varchar(100),
+    task_instance        varchar(100),
+    task_data            blob,
+    execution_time       TIMESTAMP(6),
+    picked               NUMBER(1, 0),
+    picked_by            varchar(50),
+    last_success         TIMESTAMP(6),
+    last_failure         TIMESTAMP(6),
+    consecutive_failures NUMBER(19, 0),
+    last_heartbeat       TIMESTAMP(6),
+    version              NUMBER(19, 0),
     PRIMARY KEY (task_name, task_instance)
-);
+)
