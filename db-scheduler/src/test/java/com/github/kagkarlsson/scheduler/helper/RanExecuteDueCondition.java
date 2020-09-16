225b056fd8064a1dc9e471ea607580e49bfe84dc
Gustav Karlsson <gustav.karlsson@bekk.no>
2020-09-16 13:16:23 +0200

    Some debug-logging for wait-conditions in tests

--- db-scheduler/src/test/java/com/github/kagkarlsson/scheduler/helper/RanExecuteDueCondition.java
+++ db-scheduler/src/test/java/com/github/kagkarlsson/scheduler/helper/RanExecuteDueCondition.java
@@ -1,20 +1,24 @@
 package com.github.kagkarlsson.scheduler.helper;
 
 import com.github.kagkarlsson.scheduler.stats.StatsRegistry;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CountDownLatch;
 
 public class RanExecuteDueCondition implements TestableRegistry.Condition {
 
     private final CountDownLatch count;
+    private final int waitForCount;
 
     public RanExecuteDueCondition(int waitForCount) {
         count = new CountDownLatch(waitForCount);
+        this.waitForCount = waitForCount;
     }
 
     @Override
     public void waitFor() {
         try {
+            LoggerFactory.getLogger(RanExecuteDueCondition.class).info("Starting await for "+waitForCount+" ExecutionCompleted");
             count.await();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
@@ -24,6 +28,7 @@ public class RanExecuteDueCondition implements TestableRegistry.Condition {
     @Override
     public void apply(StatsRegistry.SchedulerStatsEvent e) {
         if (e == StatsRegistry.SchedulerStatsEvent.RAN_EXECUTE_DUE) {
+            LoggerFactory.getLogger(RanExecuteDueCondition.class).info("Received event executed-due, counting down");
             count.countDown();
         }
     }
