84b16620015ed1fcb13e930d3f2a01c4799606d9
Gustav Karlsson <gustav.karlsson@bekk.no>
2020-11-14 21:02:46 +0100

    Test verifying that Spring can manage the transactions for the SchedulerClient.

--- examples/spring-boot-example/src/test/java/com/github/kagkarlsson/examples/boot/SmokeTest.java
+++ examples/spring-boot-example/src/test/java/com/github/kagkarlsson/examples/boot/SmokeTest.java
@@ -1,31 +1,50 @@
 package com.github.kagkarlsson.examples.boot;
 
+import static java.time.Duration.ofDays;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.github.kagkarlsson.scheduler.Scheduler;
 import com.github.kagkarlsson.scheduler.boot.actuator.DbSchedulerHealthIndicator;
 import com.github.kagkarlsson.scheduler.task.Task;
+import com.github.kagkarlsson.scheduler.task.TaskInstance;
 import com.github.kagkarlsson.scheduler.task.helper.OneTimeTask;
 import com.github.kagkarlsson.scheduler.task.helper.RecurringTask;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.assertj.AssertableWebApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
+
+import java.time.Duration;
+import java.time.Instant;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SmokeTest {
+    private static final Logger LOG = LoggerFactory.getLogger(SmokeTest.class);
+
+    AssertableWebApplicationContext ctx;
     @Autowired
     ConfigurableWebApplicationContext applicationContext;
-    AssertableWebApplicationContext ctx;
-
     @Autowired
     DbSchedulerHealthIndicator healthIndicator;
+    @Autowired
+    Task<Void> sampleOneTimeTask;
+    @Autowired
+    Scheduler scheduler;
+    @Autowired
+    TransactionTemplate tt;
 
     @BeforeEach
     public void setup() {
@@ -53,4 +72,25 @@ public class SmokeTest {
     public void it_should_be_healthy_after_startup() {
         assertThat(healthIndicator.health().getStatus()).isEqualTo(Status.UP);
     }
+
+    @Test
+    public void it_should_manage_transactions_for_scheduler_client() {
+        final TaskInstance<Void> instance1 = sampleOneTimeTask.instance("1");
+        final TaskInstance<Void> instance2 = sampleOneTimeTask.instance("2");
+        scheduler.schedule(instance1, Instant.now().plus(ofDays(1)));
+        assertTrue(scheduler.getScheduledExecution(instance1).isPresent());
+
+        try {
+            tt.execute(status -> {
+                scheduler.schedule(instance2, Instant.now().plus(ofDays(1)));
+                throw new SimulatedException();
+            });
+        } catch (SimulatedException e) {
+            LOG.info("Caught expected SimulatedException. Should rollback ongoing transation for instance2");
+        }
+
+        assertFalse(scheduler.getScheduledExecution(instance2).isPresent(), "instance2 should have been rolled back");
+    }
+
+    static class SimulatedException extends RuntimeException {}
 }
