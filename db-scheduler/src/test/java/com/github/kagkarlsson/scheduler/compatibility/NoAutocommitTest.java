975d4ea402781beb5082638eace65d004104294a
Gustav Karlsson <gustav.karlsson@bekk.no>
2021-05-05 22:30:49 +0200

    Use testcontainer for NoAutocommitTest

--- db-scheduler/src/test/java/com/github/kagkarlsson/scheduler/compatibility/NoAutocommitTest.java
+++ db-scheduler/src/test/java/com/github/kagkarlsson/scheduler/compatibility/NoAutocommitTest.java
@@ -1,6 +1,6 @@
-package com.github.kagkarlsson.scheduler.functional;
+package com.github.kagkarlsson.scheduler.compatibility;
 
-import com.github.kagkarlsson.scheduler.EmbeddedPostgresqlExtension;
+import com.github.kagkarlsson.scheduler.DbUtils;
 import com.github.kagkarlsson.scheduler.ScheduledExecution;
 import com.github.kagkarlsson.scheduler.Scheduler;
 import com.github.kagkarlsson.scheduler.SchedulerName;
@@ -12,24 +12,58 @@ import com.github.kagkarlsson.scheduler.task.helper.Tasks;
 import com.github.kagkarlsson.scheduler.task.schedule.Schedule;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.util.DriverDataSource;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Optional;
+import java.util.Properties;
 
 import static co.unruly.matchers.OptionalMatchers.empty;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
+@Tag("compatibility")
+@Testcontainers
 public class NoAutocommitTest {
 
-    @RegisterExtension
-    public EmbeddedPostgresqlExtension postgres = new EmbeddedPostgresqlExtension();
+    private static HikariDataSource noAutocommitingDatasource;
     @RegisterExtension
     public StopSchedulerExtension stopScheduler = new StopSchedulerExtension();
 
+    @Container
+    private static final PostgreSQLContainer POSTGRES = new PostgreSQLContainer();
+
+    @BeforeAll
+    private static void initSchema() {
+        final DriverDataSource datasource = new DriverDataSource(POSTGRES.getJdbcUrl(), "org.postgresql.Driver",
+            new Properties(), POSTGRES.getUsername(), POSTGRES.getPassword());
+
+        // init schema
+        DbUtils.runSqlResource("/postgresql_tables.sql").accept(datasource);
+
+        // Setup non auto-committing datasource
+        final HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDataSource(datasource);
+        hikariConfig.setAutoCommit(false);
+        noAutocommitingDatasource = new HikariDataSource(hikariConfig);
+    }
+
+    @AfterEach
+    public void clearTables() {
+        assertTimeoutPreemptively(Duration.ofSeconds(20), () ->
+            DbUtils.clearTables(noAutocommitingDatasource)
+        );
+    }
 
     @Test
     public void onstartup_should_always_commit() {
@@ -38,12 +72,7 @@ public class NoAutocommitTest {
             .execute(counter);
 
         // Setup non auto-committing datasource
-        final HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDataSource(postgres.getNonPooledDatasource());
-        hikariConfig.setAutoCommit(false);
-        HikariDataSource noAutocommitDatasource = new HikariDataSource(hikariConfig);
-
-        Scheduler scheduler = Scheduler.create(noAutocommitDatasource)
+        Scheduler scheduler = Scheduler.create(noAutocommitingDatasource)
             .startTasks(recurring1)
             .schedulerName(new SchedulerName.Fixed("test"))
             .commitWhenAutocommitDisabled(false)
