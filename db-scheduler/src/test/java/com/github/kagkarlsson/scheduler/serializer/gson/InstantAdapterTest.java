f1ab2a9c7e44e4d6655b02bea7c942ca34d39ae5
Gustav Karlsson <kagkarlsson@gmail.com>
2022-05-04 15:28:39 +0200

    Serialization review

--- db-scheduler/src/test/java/com/github/kagkarlsson/scheduler/serializer/gson/InstantAdapterTest.java
+++ db-scheduler/src/test/java/com/github/kagkarlsson/scheduler/serializer/gson/InstantAdapterTest.java
@@ -19,10 +19,4 @@ public class InstantAdapterTest {
         assertEquals(now, gson.fromJson(gson.toJson(now), Instant.class));
     }
 
-    @Test
-    public void serialize_duration() {
-        final Duration duration = Duration.ofSeconds(10);
-        final Gson gson = GsonSerializer.getDefaultGson().create();
-        assertEquals(duration, gson.fromJson(gson.toJson(duration), Duration.class));
-    }
 }
