eaa32e7f8b7460837b65888f158126e67fd93e4f
quaso <quaso@users.noreply.github.com>
2023-01-31 09:48:02 +0100

    fixes #354 MSSQL task_data datatype

--- db-scheduler/src/test/resources/mssql_tables.sql
+++ db-scheduler/src/test/resources/mssql_tables.sql
@@ -1,7 +1,7 @@
 create table scheduled_tasks (
   task_name varchar(250) not null,
   task_instance varchar(250) not null,
-  task_data  nvarchar(max),
+  task_data varbinary(max),
   execution_time datetimeoffset  not null,
   picked bit,
   picked_by text,
