483f8246c9b93777cfd8003cd679978f9facf19e
Ilia Koliaskin <kolyaskin.ilya@gmail.com>
2025-08-01 09:53:59 +0200

    Feature: add @RecurringTask annotation to help with task creation in a Spring environment

---
+++ db-scheduler-boot-starter/src/main/java/com/github/kagkarlsson/scheduler/boot/autoconfigure/RecurringTask.java
@@ -0,0 +1,21 @@
+package com.github.kagkarlsson.scheduler.boot.autoconfigure;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Scheduled tasks are created from the methods that are marked with this annotation. The method
+ * must follow these rules: - it returns void - it has 0, 1 or 2 inputs with the following types:
+ * {@link com.github.kagkarlsson.scheduler.task.TaskInstance}, generic is ignored and considered
+ * Void {@link com.github.kagkarlsson.scheduler.task.ExecutionContext}
+ */
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RecurringTask {
+
+  String name();
+
+  String cron() default "-";
+}
