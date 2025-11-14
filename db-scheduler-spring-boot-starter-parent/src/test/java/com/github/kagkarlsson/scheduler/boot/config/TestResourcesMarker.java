6915e525b57594fbb26a1ad523b3ab2fa1cb472b
Julien Lafourcade <julien.lafourcade@asso-cocktail.fr>
2025-11-14 09:26:17 +0100

    test: share schema.sql/resources via parent test-jar

---
+++ db-scheduler-spring-boot-starter-parent/src/test/java/com/github/kagkarlsson/scheduler/boot/config/TestResourcesMarker.java
@@ -0,0 +1,6 @@
+package com.github.kagkarlsson.scheduler.boot.config;
+
+/** Marker class so Maven can produce a test-jar exposing shared test resources. */
+public final class TestResourcesMarker {
+  private TestResourcesMarker() {}
+}
