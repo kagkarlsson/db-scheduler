324a17c24eb6f464c37435d309f4236f88b301bc
Julien Lafourcade <julien.lafourcade@asso-cocktail.fr>
2025-11-14 09:33:02 +0100

    test: move shared schema to boot test support module

--- db-scheduler-spring-boot-starter-parent/src/test/java/com/github/kagkarlsson/scheduler/boot/config/TestResourcesMarker.java
+++
@@ -1,6 +0,0 @@
-package com.github.kagkarlsson.scheduler.boot.config;
-
-/** Marker class so Maven can produce a test-jar exposing shared test resources. */
-public final class TestResourcesMarker {
-  private TestResourcesMarker() {}
-}
