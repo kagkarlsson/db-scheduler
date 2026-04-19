753f045bcb9227c5ca257bcdc29f26ec83d2bac7
Gustav Karlsson <kagkarlsson@gmail.com>
2026-04-19 22:54:32 +0200

    Revert changes to MariaDB and MySQL

--- db-scheduler/src/test/java/com/github/kagkarlsson/scheduler/compatibility/Mysql8CompatibilityTest.java
+++ db-scheduler/src/test/java/com/github/kagkarlsson/scheduler/compatibility/Mysql8CompatibilityTest.java
@@ -1,11 +1,16 @@
 package com.github.kagkarlsson.scheduler.compatibility;
 
 import com.github.kagkarlsson.scheduler.DbUtils;
+import com.github.kagkarlsson.scheduler.jdbc.AutodetectJdbcCustomization;
+import com.github.kagkarlsson.scheduler.jdbc.JdbcCustomization;
+import com.github.kagkarlsson.scheduler.jdbc.MySQL8JdbcCustomization;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import com.zaxxer.hikari.util.DriverDataSource;
+import java.util.Optional;
 import java.util.Properties;
 import javax.sql.DataSource;
+import javax.swing.text.html.Option;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.testcontainers.containers.MySQLContainer;
@@ -55,5 +60,10 @@ public class Mysql8CompatibilityTest extends CompatibilityTest {
   @Override
   public boolean commitWhenAutocommitDisabled() {
     return false;
+  }
+
+  @Override
+  public Optional<JdbcCustomization> getJdbcCustomization() {
+    return Optional.of(new AutodetectJdbcCustomization(getDataSource(), true));
   }
 }
